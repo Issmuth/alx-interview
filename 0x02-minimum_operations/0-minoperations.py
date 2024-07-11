14e1dd625979314c887272a7cdfed13f958a0237
issmuth <ismael.mdev@gmail.com>
2024-07-11 23:51:40 +0300

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,14 @@ def minOperations(n):
     """find the minimum number of operations,
     to print n number of characters using the
     operations copy all and past only."""
-    if num < 2:
+    if n < 2:
         return 0
     
     operations, divisor = 0, 2
-    for divisor in range(2, num + 1):
-        while num % divisor == 0:
+    for divisor in range(2, n + 1):
+        while n % divisor == 0:
             operations += divisor
-            num //= divisor
+            n //= divisor
         divisor -= 1
     
     return operations
