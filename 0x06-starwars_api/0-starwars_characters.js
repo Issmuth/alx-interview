3faefb6700e31b52ef7f442756401143f9fd15cf
issmuth <ismael.mdev@gmail.com>
2024-08-06 22:16:43 +0300

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,8 +2,32 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function promisify (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+
+async function findName (characters) {
+  const characterNames = [];
+  for (const character of characters) {
+    try {
+      const charac = await promisify(character);
+      characterNames.push(charac.name);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  }
+  return characterNames;
+}
 
 request(url, (error, reponse, body) => {
   if (error) {
@@ -11,13 +35,10 @@ request(url, (error, reponse, body) => {
   }
   const data = JSON.parse(body);
   const characters = data.characters;
-  characters.forEach(character => {
-    request(character, (error2, response2, body2) => {
-      if (error2) {
-        console.log('Error: ' + error2);
-      }
-      const person = JSON.parse(body2);
-      console.log(person.name);
+
+  findName(characters).then(characterNames => {
+    characterNames.forEach(character => {
+      console.log(character);
     });
   });
 });
