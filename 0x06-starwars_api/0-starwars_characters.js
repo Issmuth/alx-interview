7828dd72f3fdd9e73b46a3fe3a4bba54027ce082
issmuth <ismael.mdev@gmail.com>
2024-08-06 21:02:47 +0300

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const { json } = require('stream/consumers');
 
-const movieId = process.argv[2]
+const movieId = process.argv[2];
 
-
-const url = "https://swapi-api.alx-tools.com/api/films/" + movieId;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(url, (error, reponse, body) => {
-    const data = JSON.parse(body);
-    characters = data["characters"];
-    characters.forEach(character => {
-        request(character, (error, response2, body2) => {
-            const person = JSON.parse(body2);
-            console.log(person["name"]);
-        });
+  if (error) {
+    console.log('Error: ' + error);
+  }
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  characters.forEach(character => {
+    request(character, (error2, response2, body2) => {
+      if (error2) {
+        console.log('Error: ' + error2);
+      }
+      const person = JSON.parse(body2);
+      console.log(person.name);
     });
+  });
 });
