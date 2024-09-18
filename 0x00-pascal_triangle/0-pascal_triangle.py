fe9905303e8c293c7920c2987f6415cfd9622095
issmuth <ismael.mdev@gmail.com>
2024-09-18 12:42:26 +0300

    making change completed

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Pascal Triangle Representaion."""
+
+def pascal_triangle(n):
+    triangle = [[1],[1,1]]
+
+    if n <= 0:
+        return []
+    
+    if n == 1:
+        return [[1]]
+    
+    if n == 2:
+        return triangle
+
+    if n % 2 != 0:
+        d6t
+
+    
