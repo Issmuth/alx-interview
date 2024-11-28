2de42297e284044e30d4afe42ff344e19b63a2c2
issmuth <ismael.mdev@gmail.com>
2024-11-28 18:59:38 +0300

    simpler fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """Least number of coins in change."""
+import math
 
 
 def makeChange(coins, total):
     """returns the least number
     of coins needed for change."""
     coinsCount = 0
+    
+    if total == 0:
+        return 0
 
     coins.sort()
-
     i = len(coins) - 1
-    while total > 0 and i >= 0:
-        if total % coins[i] == 0:
-            return int((total / coins[i]) + coinsCount)
 
-        total -= coins[i]
-        coinsCount += 1
+    while i >= 0 and total > 0:
+        if coins[i] < total:
+            coinsCount += math.floor(total / coins[i])
+            total = total % coins[i]
 
-        if coins[i] > total:
-            i -= 1
-
-    if total < 0:
+        i -= 1
+    
+    if total == 0:
+        return coinsCount
+    else:
         return -1
-
-    return int(coinsCount)
